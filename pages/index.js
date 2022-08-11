70f8392cb23dae2ac8648194c3d8af1b5f8990af
Costeira <joaocurveira13@gmail.com>
2022-08-11 12:15:26 +0100

    header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Head>
+        <title>Hulu Clone</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <Header/>
+      {/**Navbar */}
+      {/**Results */}
+
+    </div>
   )
 }
